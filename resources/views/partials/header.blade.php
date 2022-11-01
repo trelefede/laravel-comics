1004d1fecd5457242714a3049d6e436871aa3b07
TreleaniFederico <treleani.federico@gmail.com>
2022-11-01 11:34:42 +0100

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,29 @@
 <header>
-    <img src=" {{ asset('') }} " alt="">
+    <div class="top-header">
+        <div class="container">
+            <a href="">DC POWER VISA</a>
+            <a href="">ADDITIONAL DC SITES</a>
+        </div>
+    </div>
+    <div class="container">
+        <nav>
+            <img src=" {{ asset('images/dc-logo.png') }} " alt="dc-logo">
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+            <input type="text" placeholder="Search">
+        </nav>
+    </div>
+
 
 
 </header>
