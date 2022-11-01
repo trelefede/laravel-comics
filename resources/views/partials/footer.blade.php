9146db9d894f46502fae445ca47f1fe433bb109b
TreleaniFederico <treleani.federico@gmail.com>
2022-11-01 13:17:27 +0100

    add footer-top

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,69 @@
+<footer>
+    <div class="footer-top">
+        <div class="container">
+
+            {{-- links section --}}
+            <div class="links">
+                <div>
+                    <div>
+                        <h5>DC COMICS</h5>
+                        <ul>
+                            <li><a href="">Characters</li></a>
+                            <li><a href="">Comics</li></a>
+                            <li><a href="">Movies</li></a>
+                            <li><a href="">TV</li></a>
+                            <li><a href="">Games</li></a>
+                            <li><a href="">Videos</li></a>
+                            <li><a href="">News</li></a>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>SHOP</h5>
+                        <ul>
+                            <li><a href="">Shop DC</li></a>
+                            <li><a href="">Shop DC Collectibles</li></a>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h5>DC</h5>
+                    <ul>
+                        <li><a href="">Terms Of Use</li></a>
+                        <li><a href="">Privacy Policy (New)</li></a>
+                        <li><a href="">Ad Choices</li></a>
+                        <li><a href="">Advertising</li></a>
+                        <li><a href="">Jobs</li></a>
+                        <li><a href="">Subscriptions</li></a>
+                        <li><a href="">Talent Workshop</li></a>
+                        <li><a href="">CPSC Certificates</li></a>
+                        <li><a href="">Ratings</li></a>
+                        <li><a href="">Shop Help</li></a>
+                        <li><a href="">Contact Us</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <h5>SITES</h5>
+                    <ul>
+                        <li><a href="">DC</li></a>
+                        <li><a href="">MAD Magazine</li></a>
+                        <li><a href="">DC Kids</li></a>
+                        <li><a href="">DC Universe</li></a>
+                        <li><a href="">DC Power Visa</li></a>
+                    </ul>
+                </div>
+            </div>
+            {{-- end links section --}}
+
+            <p>All Site Content TM and c 2020 DC Entertainment, unless otherwise <a href="">noted here</a>. All
+                rights reserved. <br><a href="">Cookies Settings</a></p>
+
+            <img src=" {{ asset('images/dc-logo-bg.png') }} " alt="dc-logo-bg">
+
+        </div>
+        {{-- end container --}}
+
+    </div>
+    {{-- end footer-top --}}
+
+
+</footer>
