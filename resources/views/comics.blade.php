06944127e3199faecffce618d56daa335b829f45
TreleaniFederico <treleani.federico@gmail.com>
2022-11-01 16:53:51 +0100

    add comics by route

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="comics-section">
+        <div class="container">
+            <div class="comics">
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <img src=" {{ $comic['thumb'] }} " alt=" {{ $comic['title'] }} ">
+                        <h3>
+                            <a href=""> {{ $comic['series'] }} </a>
+                        </h3>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+    $comics = config('data.comics');
+    $data = [
+        'comics' => $comics
+    ];
+    return view('comics', $data);
 });
